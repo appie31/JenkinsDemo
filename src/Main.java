98f4778b87679ec93020982e21f490f215c1f19b
Aparna Kumari <aparnakumari1602@gmail.com>
2025-04-20 14:30:18 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, VS Code Java Project!");
+        System.out.println("This is a demo JAVA project to run from Jenkins");
+        System.out.println("Jenkins first project , build using Jenkins");
+    }
+}
