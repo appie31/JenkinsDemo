74089154df592cb53d88fafdcf1443bd8288d5e4
Aparna Kumari <aparnakumari1602@gmail.com>
2025-04-20 14:50:12 +0530

    2nd update commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, VS Code Java Project!");
         System.out.println("This is a demo JAVA project to run from Jenkins");
-        System.out.println("Jenkins first project , build using Jenkins");
+        System.out.println("Jenkins first project , build using Jenkinsgi");
+        System.out.println("second commit");
     }
 }
